50bee7e8a5e0217dce783ea8c7116d5d6df9fe55
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-06-16 17:02:04 -0300

    feat/config migrations

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,25 @@
-import {knex as setupKnex} from 'knex'
+import {knex as setupKnex, Knex} from 'knex'
 
 //Configuração do banco de dados, salva em um arquivo sqlite na pasta tmp
-export const knex = setupKnex({
+
+//migrations: são arquivos que contém as alterações do banco de dados, para controlar as versões do banco de dados
+
+//npm run knex -- migrate:make nome_da_migration
+
+//npm i knex sqlite3
+
+
+export const config: Knex.Config = {
     client: 'sqlite',
     connection: {
-        filename: './tmp/app.db'
+        filename: './db/app.db'
+    },
+    useNullAsDefault: true, // Para não precisar definir valores nulos para cada campo
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
     }
-})
+    
+}
+
+export const knex = setupKnex(config)
