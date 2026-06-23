dce48fda70e359344f23d16c30e62ed6a1b30d3b
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-06-23 15:34:07 -0300

    feat/user cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,30 +5,57 @@ import {
   createTransactionBodySchema,
   getTransactionParamsSchema,
 } from "../schemas/transactionsSchemas";
-
-
+import { checkSessionIdExist } from "../middlewares/check-session-id-exist";
 
 export async function transactionsRoutes(app: FastifyInstance) {
   //resumo das transações, soma todas as transações de credito e retorna o total, as: "amount" é o nome da coluna que será retornada, nao precisa ser o mesmo nome da coluna do banco de dados.
-  app.get("/summary", async () => {
-    const summary = await knex("transactions")
-      .sum("amount", { as: "amount" })
-      .first();
-    return { summary };
-  });
+  app.get(
+    "/summary",
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+      const summary = await knex("transactions")
+        .where("session_id", sessionId)
+        .sum("amount", { as: "amount" })
+        .first();
+      return { summary };
+    },
+  );
 
-  //listar todas as transações
-  app.get("/", async () => {
-    const transactions = await knex("transactions").select();
-    return { transactions };
-  });
+  //listar todas as transações, preHandler é um middleware que será executado antes da rota, no caso, verificar se a sessionId existe.
+  app.get(
+    "/",
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
 
-  //listar uma transação específica
-  app.get("/:id", async (request) => {
-    const { id } = getTransactionParamsSchema.parse(request.params);
-    const transaction = await knex("transactions").where("id", id).first();
-    return { transaction };
-  });
+      //Só ira listar as transações que pertencem a sessão do usuário
+      const transactions = await knex("transactions")
+        .where("session_id", sessionId)
+        .select();
+      return { transactions };
+    },
+  );
+
+  //listar uma transação específica, preHandler é um middleware que será executado antes da rota, no caso, verificar se a sessionId existe.
+  app.get(
+    "/:id",
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+      const { id } = getTransactionParamsSchema.parse(request.params);
+      const transaction = await knex("transactions")
+        .where({ id, session_id: sessionId })
+        .first();
+      return { transaction };
+    },
+  );
 
   //criar uma transação
   app.post("/", async (request, reply) => {
