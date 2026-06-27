b41562b581058bfdeaa6125c0e12b93f15f9950f
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-06-27 13:57:56 -0300

    feat/automatic testing

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,14 @@ import { env } from "./env";
 import { transactionsRoutes } from "./routes/transactions";
 import fastifyCookie from "@fastify/cookie";
 
+//Teste unitarios = testes que testam uma unidade de codigo, uma funcao, uma classe, etc.
+
+//Teste de integração = testes que testam a integracao entre duas ou mais unidades de codigo, uma funcao, uma classe, etc.
+
+//Teste e2e - end to end = testes que testam a integracao entre a aplicacao e o ambiente externo, como um banco de dados, um servidor, etc. Simula um usuario usando a aplicacao.
+
+//Piramide de testes = Teste Unitários, Depois (Integracao), Depois (E2E)
+
 const app = fastify();
 
 //npm i @fastify/cookie
