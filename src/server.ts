7c5b908ea8cb3d415aada9ccc39f67fdf9a5f74c
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-06-30 15:05:08 -0300

    feat/config production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { env } from "./env";
 import { app } from "./app";
 
+//npm i tsup -D - usado para compilar o projeto para produção, converter o código typescript para javascript
+
 app.listen({ port: env.PORT }).then(() => {
     console.log(`Server is running on port ${env.PORT}`);
 });
